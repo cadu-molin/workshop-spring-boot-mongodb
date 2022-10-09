aeddf1f4529593236e53cacecb0e644d73feed72
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2022-10-09 12:44:08 -0300

    Insercao de usuario com POST

--- src/main/java/com/carlosmolin/workshopmongo/resources/UserResource.java
+++ src/main/java/com/carlosmolin/workshopmongo/resources/UserResource.java
@@ -1,14 +1,14 @@
 package com.carlosmolin.workshopmongo.resources;
 
+import com.carlosmolin.workshopmongo.domain.User;
 import com.carlosmolin.workshopmongo.dto.UserDTO;
 import com.carlosmolin.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,5 +30,12 @@ public class UserResource {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserDTO> findAll(@PathVariable String id) {
         return ResponseEntity.ok().body(new UserDTO(userService.findById(id)));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User obj = userService.insert(userService.fromDTO(userDTO));
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/carlosmolin/workshopmongo/services/UserService.java
+++ src/main/java/com/carlosmolin/workshopmongo/services/UserService.java
@@ -1,8 +1,10 @@
 package com.carlosmolin.workshopmongo.services;
 
 import com.carlosmolin.workshopmongo.domain.User;
+import com.carlosmolin.workshopmongo.dto.UserDTO;
 import com.carlosmolin.workshopmongo.repositories.UserRepository;
 import com.carlosmolin.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +22,13 @@ public class UserService {
 
     public User findById(String id) {
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
